221c11c08b2388f34bf8b39ca8a350a317f1bba3
aljumailyz <zaidusama2001@icloud.com>
2025-11-21 16:44:46 +0300

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// supabaseClient.js
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-
-const SUPABASE_URL = 'https://xsrbsmjklnigrtmqkbsm.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_d82AZMNDIcPtyJUJECehRw_Rf2ZMfXV';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
