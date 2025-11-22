7a467f317ec002e2bd15f89566e3cd04b457a27a
aljumailyz <zaidusama2001@icloud.com>
2025-11-22 13:53:22 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase public config
+window.__SUPABASE_CONFIG = {
+  supabaseUrl: 'https://wttfyubbhjtviixueaje.supabase.co',
+  supabaseAnonKey: 'sb_publishable_hJrvlg9uF8w9gdqzuY1Atw_gn-4dRC-',
+};
